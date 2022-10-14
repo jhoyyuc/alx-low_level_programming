53803ec830f55bbd2a9d9bbc9fd54c37e1049f9d
Obisike Uchechi Joy <obisikeuchechijoy@gmail.com>
2022-10-13 21:39:31 -0700

    working on

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include,stdio.h>
+/**
+ * *main- Seerves as the entry point for the program
+ * *
+ * *Return: By defaultreturns zero if no errors otherwise returns non-zero value.
+ * */
+int main(void)
+{
+	 puts("\"programming is like building a multilingual puzzle");
+	  return (0);
+}
+
