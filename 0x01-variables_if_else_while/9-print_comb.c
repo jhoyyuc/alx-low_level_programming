11d6713bb3563072de4b674ff51f5c289accb0af
Obisike Uchechi Joy <obisikeuchechijoy@gmail.com>
2022-10-15 22:59:37 -0700

    retrying

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,18 @@
 int main(void)
 {
 int num;
-
 for (num = '0'; num <= '9'; num++)
 {
-putchar(num);
-										if (num == '9')									{
+putchar(num);									}
+if (num == '9')
+{
 break;
-}										else										{					
-putchar(',');									putchar(' ');									}			
+}
+else
+{
+putchar(',');									putchar(' ');									}
 }
 putchar('\n');
+{
 return (0);
 }
