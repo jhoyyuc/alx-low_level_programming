ca9c0f7ec659ae13a2c218f1f3c017d3d400b96f
Obisike Uchechi Joy <obisikeuchechijoy@gmail.com>
2022-10-15 23:55:32 -0700

    Pushing it

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,20 @@
 int main(void)
 {
 int num;
+
 for (num = '0'; num <= '9'; num++)
 {
-putchar(num);									}
+putchar(num);
 if (num == '9')
 {
 break;
 }
 else
 {
-putchar(',');									putchar(' ');									}
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
-{
 return (0);
 }
