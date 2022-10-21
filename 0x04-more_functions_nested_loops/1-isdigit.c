85f45a835bc5850643831b7f49ebcbb65aa1efdf
Obisike Uchechi Joy <obisikeuchechijoy@gmail.com>
2022-10-21 14:04:01 -0700

    I am an alx student

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ *
+ * @c: character to be tested
+ * Return: Always 0 (success)
+ */
+int _isdigit(int c);
+{
+	if (c >= '0' && C <= '9')
+		return (1);
+	else
+		return (0);
+}	
