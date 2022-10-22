12eb6119b9c460d9a6f3a93baa31c414dc3c3596
Obisike Uchechi Joy <obisikeuchechijoy@gmail.com>
2022-10-21 21:35:53 -0700

    Just Fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-#include <stdio.h>
 
 /**
- * main - print a num from 1 to 100 followed by a new line
- * 3 print fizz
- * 5 print buzz
- * 3 and 5 print fizzbuzz
- * Return: Always
- */
+* main - Entry point
+*
+* Return: 0  always
+*/
 int main(void)
 {
-	int i;
-	char f[] = "FIZZ";
-	char b[] = "BUZZ";
-	char fb[] = "FizzBuzz";
-
-	for (i = 1; i <= 100; i++)
-	{
-	if (i == 100)
-		printf("%5 ", b);
-	else if ((i % 3 == 0) && (i % 5 == 0))
-		printf("%5 ", fb);
-	}
-	return (0);
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 != 0 && i % 5 != 0)
+printf("%d ", i);
+if (i % 3 == 0 && i % 5 != 0)
+printf("Fizz ");
+if (i % 3 != 0 && i % 5 == 0)
+{
+if (i == 100)
+printf("Buzz");
+else
+printf("Buzz ");
+}
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+}
+putchar('\n');
+return (0);
 }
